613aed12469c2fa24f31c985278454594934616c
ArkStuff <apr9131@gmail.com>
2025-06-09 08:35:29 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the Encode Bootcamp FizzBuzz Challenge!");
+    fizz_buzz();
+}
+
+fn fizz_buzz() {
+    let mut fizz_buzz_count = 0;
+
+    for i in 1..=301 {
+        if i % 15 == 0 {
+            println!("fizz buzz");
+            fizz_buzz_count += 1;
+        } else if i % 3 == 0 {
+            println!("fizz");
+        } else if i % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", i);
+        }
+    }
+
+    println!("Total number of 'fizz buzz' occurrences: {}", fizz_buzz_count);
 }
